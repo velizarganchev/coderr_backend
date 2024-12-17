65d9a4148044c89601b9604208d5a3ec9668f1e1
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-12-17 21:34:27 +0100

    Add user initialization logic, update user model defaults, and enhance offer image migration

--- user_auth_app/apps.py
+++ user_auth_app/apps.py
@@ -1,4 +1,6 @@
+import os
 from django.apps import AppConfig
+import logging
 
 
 class UserAuthAppConfig(AppConfig):
@@ -7,3 +9,37 @@ class UserAuthAppConfig(AppConfig):
 
     def ready(self):
         import user_auth_app.signals
+        self.check_and_create_users()
+
+    def check_and_create_users(self):
+        """Prüft, ob die Benutzer existieren, und erstellt sie, falls nicht."""
+        from django.contrib.auth.models import User
+        from rest_framework.authtoken.models import Token
+
+        flag_file = os.path.join(os.path.dirname(__file__), 'initialized.flag')
+
+        if not os.path.exists(flag_file):
+            users_to_create = [
+                {"username": "andrey", "password": "asdasd",
+                    'type': 'customer', "is_staff": False},
+                {"username": "kevin", "password": "asdasd24",
+                    'type': 'business', "is_staff": True},
+            ]
+
+            for user_data in users_to_create:
+                if not User.objects.filter(username=user_data["username"]).exists():
+                    user = User.objects.create_user(
+                        username=user_data["username"],
+                        password=user_data["password"],
+                        is_staff=user_data.get("is_staff", False),
+                    )
+                    user.userprofile.type = user_data["type"]
+                    user.userprofile.save()
+                    Token.objects.create(user=user)
+                    logging.info(f"User {user_data['username']} created.")
+                else:
+                    logging.info(
+                        f"User {user_data['username']} already exists.")
+
+            with open(flag_file, 'w') as f:
+                f.write('initialized')
--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -11,10 +11,13 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     file = models.ImageField(upload_to='profile_images', blank=True, null=True)
-    location = models.CharField(max_length=100, blank=True, null=True)
-    tel = models.CharField(max_length=100, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    working_hours = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(
+        max_length=100, blank=True, null=True, default='Unknown')
+    tel = models.CharField(max_length=100, blank=True,
+                           null=True, default='Unknown')
+    description = models.TextField(blank=True, null=True, default='Unknown')
+    working_hours = models.CharField(
+        max_length=100, blank=True, null=True, default='Unknown')
     type = models.CharField(
         max_length=20, choices=USER_TYPE_CHOICES, default='customer')
 
